0de6d559a68c83449b236c0c38fad1b0d254b34a
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-02-17 13:53:36 +0100

    - Add my_world.h: struct my_world + define

--- include/my_world.h
+++ include/my_world.h
@@ -18,6 +18,16 @@
 
 #define ANGLE_X 0.785398
 #define ANGLE_Y 0.610865
+#define MAP_X 10
+#define MAP_Y 10
+#define DISPLAY_X 32
+#define DISPLAY_Y 32
+
+typedef struct my_world_s {
+    int **map;
+    sfVector2f scale;
+    sfVector2f pos;
+} my_world_t;
 
 int usage(int exit_value, char *binary_name);
 bool does_kill_prog(assets_t *assets);
