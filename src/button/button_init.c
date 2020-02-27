acd3cd7c941c729d4cc22a29d050cc02fc459b6f
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-02-27 21:32:25 +0100

    - Fix style in button_init.c

--- src/button/button_init.c
+++ src/button/button_init.c
@@ -53,7 +53,7 @@ static void texture_set(sfTexture **texture, button_manage_t *button)
 
 static int set_name_struct(button_manage_t *button)
 {
-    char *name[]= { "EXIT", "RESTART", "ZOOM +", "ZOOM -", "X +", "X -", "Y +",
+    char *name[] = { "EXIT", "RESTART", "ZOOM +", "ZOOM -", "X +", "X -", "Y +",
                     "Y -", "UP", "DOWN", "LEFT", "RIGHT", "LOAD", "SAVE",
                     "TOOLS"};
     button_manage_t *manage = button;
